fff232b7b3974b85fe7c32a7a1b0dcff8ecc5dc9
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-10-30 12:13:14 -0400

    LEDS go around the edges of microbit

--- main.ts
+++ main.ts
@@ -1,8 +1,72 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Scout Mosley
+ * Created on: SOct 2025
+ * This program shows LEDS light around the edges.
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounter = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+// when "A" is pressed, the pixels around the edge
+input.onButtonPressed(Button.A, function () {
+
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+
+    // across top right
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(300)
+    }
+    sprite.delete()
+
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(4, 0)
+
+    //down right side
+    while (loopCounter <= 4) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(300)
+    }
+    sprite.delete()
+
+    // setup
+    basic.clearScreen()
+    loopCounter = 5
+    sprite = game.createSprite(5, 5)
+ 
+    // across bottom left
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(300)
+    }
+    sprite.delete()
+
+    // setup
+    basic.clearScreen()
+    loopCounter = 5
+    sprite = game.createSprite(0, 4)
+
+    // up left side
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(300)
+    }
+    sprite.delete()
+
+})
