348c374d0fd2eebfae0f9ece8fb3662222af10dd
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-11-03 12:13:12 -0500

    changed comment

--- main.ts
+++ main.ts
@@ -2,71 +2,30 @@
  *
  * Created by: Scout Mosley
  * Created on: Oct 2025
- * This program shows LEDS light around the edges.
+ * This program shows count down from 4 and shows Neos.
 */
 
 // variables
-let sprite: game.LedSprite = null
-let loopCounter = 0
+let neopixelStrip: neopixel.Strip = null
+let loopCounter = 4
 
 // setup
 basic.showIcon(IconNames.Happy)
 basic.pause(500)
 basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+neopixelStrip.show()
 
-// when "A" is pressed, the pixels around the edge
+// when "A" is pressed, counts down and shows neo
 input.onButtonPressed(Button.A, function () {
-
-    // setup
-    basic.clearScreen()
-    loopCounter = 0
-    sprite = game.createSprite(0, 0)
-
-    // across top right
-    while (loopCounter <= 5) {
-        sprite.set(LedSpriteProperty.X, loopCounter)
-        loopCounter = loopCounter + 1
-        basic.pause(300)
-    }
-    sprite.delete()
-
-    // setup
-    basic.clearScreen()
-    loopCounter = 0
-    sprite = game.createSprite(4, 0)
-
-    //down right side
-    while (loopCounter <= 4) {
-        sprite.set(LedSpriteProperty.Y, loopCounter)
-        loopCounter = loopCounter + 1
-        basic.pause(300)
-    }
-    sprite.delete()
-
-    // setup
-    basic.clearScreen()
-    loopCounter = 5
-    sprite = game.createSprite(5, 5)
- 
-    // across bottom left
-    while (loopCounter >= 0) {
-        sprite.set(LedSpriteProperty.X, loopCounter)
-        loopCounter = loopCounter - 1
-        basic.pause(300)
-    }
-    sprite.delete()
-
-    // setup
-    basic.clearScreen()
-    loopCounter = 5
-    sprite = game.createSprite(0, 4)
-
-    // up left side
-    while (loopCounter >= 0) {
-        sprite.set(LedSpriteProperty.Y, loopCounter)
-        loopCounter = loopCounter - 1
-        basic.pause(300)
-    }
-    sprite.delete()
-
+while (loopCounter >= 0){
+    neopixelStrip.setPixelColor(loopCounter, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    basic.showNumber(loopCounter)
+    loopCounter = loopCounter -1
+}
 })
